5d1421d603dd94b27c3bf1cf65aa266c3c0e5b86
roller44 <roller44@163.com>
2016-04-24 21:19:08 +0800

    Add explain of line 9.

--- 2/book_code/prog2_2_4.cc
+++ 2/book_code/prog2_2_4.cc
@@ -7,5 +7,8 @@ int main()
     int reused = 0;
     std::cout << reused << " " << unique << std::endl;
     std::cout << ::reused << " " << unique << std::endl;
+    // 使用作用域操作符，因为全局作用域本身没有名字，所以作用域操作符
+    // 的左侧为空时，想全局作用域发出请求获取作用域操作符右侧名字对应
+    // 的变量
     return 0;
 }
