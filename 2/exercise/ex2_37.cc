1fb5fa89dbe7b8ed84b0654a2fcd81cba6769877
roller44 <roller44@163.com>
2016-05-03 16:11:47 +0800

    Add program of exercise 2.37.

---
+++ 2/exercise/ex2_37.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int a = 3, b = 4;
+    decltype(a) c = a;
+    decltype(a = b)  d = a;
+
+    std::cout << "a 的值是： " << a << "\n"
+        << "b 的值是： " << b << "\n"
+        << "c 的值是： " << c << "\n"
+        << "d 的值是： " << d << std::endl;
+
+    return 0;
+}
