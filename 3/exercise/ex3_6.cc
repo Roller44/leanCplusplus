f9e45515c3e15c58a9f6badadb4a46c47cf88c35
roller44 <roller44@163.com>
2016-05-06 17:01:53 +0800

    添加练习 3.6 的代码

---
+++ 3/exercise/ex3_6.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    string s, total;
+    cout << "Enter words: " << endl;
+
+    while (cin >> s) {
+        total += s;
+        if (s == "--end") {
+            break;
+        }
+    }
+    if (!total.empty()) {
+        for (auto &n : total) {
+            n = 'x';
+        }
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
