332cb6989bc05034a1052f4cc78afb58836b3cc2
roller44 <roller44@163.com>
2016-05-07 20:24:16 +0800

    修改错误

--- 3/exercise/ex3_12.cc
+++ 3/exercise/ex3_12.cc
@@ -8,7 +8,7 @@ int main()
 {
     vector<vector<int>> ivec;
    // vector<string> svec = ivec;
-   // 错误，不能够拷贝赋值
+   // 错误，不能够拷贝 int 型的 vector 对象
     vector<string> svec(10, "null");
 
     return 0;
