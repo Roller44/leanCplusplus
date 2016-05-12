2a2807a503d464de722f19131d6d0ab52d38f4f0
roller44 <roller44@163.com>
2016-05-12 22:03:33 +0800

    添加 3.5.3 代码

---
+++ 3/book_code/prog3_5_3_1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *e = &arr[10];
+    for (int *b = arr; b != e; ++b) {
+        cout << *b << endl;
+    }
+
+    return 0;
+}
