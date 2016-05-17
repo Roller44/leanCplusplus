3985fc765e08f1540fd90eb69747848eba1125a8
roller44 <roller44@163.com>
2016-05-17 19:26:46 +0800

    添加 4.2 代码

---
+++ 4/book_code/prog4_2_2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "21 % 6 = " << 21 % 6 << endl;
+    cout << "21 % 7 = " << 21 % 7 << endl;
+    cout << "-21 % -8 = " << -21 % -8 << endl;
+    cout << "21 % -5 = " << 21 % -5 << endl;
+
+    cout << "21 / 6 = " << 21 / 6 << endl;
+    cout << "21 / 7 = " << 21 / 7 << endl;
+    cout << "-21 / -8 = " << -21 / -8 << endl;
+    cout << "21 / -5 = " << 21 / -5 << endl;
+
+    return 0;
+}
