b08061a8beb5dececa5e9bfab765787fbf1b26db
roller44 <roller44@163.com>
2016-07-04 20:56:15 +0800

    添加 6.3.2 末尾代码

--- 6/book_code/prog6_3_2.cc
+++ 6/book_code/prog6_3_2.cc
@@ -9,12 +9,21 @@ char &get_val(string &str, string::size_type ix){
     return str[ix];
 }
 
+int factorial(int val){
+    if (val > 1) {
+        return factorial(val -1) * val;
+    }
+
+    return 1;
+}
+
 int main()
 {
     string s("a value");
     cout << s << endl;
     get_val(s, 0)  = 'A';
     cout << s << endl;
+    cout << factorial(5) << endl;
 
     return 0;
 }
