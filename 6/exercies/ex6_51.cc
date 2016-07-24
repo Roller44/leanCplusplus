e406835ab6a8da153559fd76e27c040a51bb38f9
roller44 <roller44@163.com>
2016-07-24 15:58:54 +0800

    添加练习 6.51 答案

---
+++ 6/exercies/ex6_51.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void f()
+{
+    cout << "This is f()." << endl;
+}
+
+void f(int)
+{
+    cout << "This is f(int)." << endl;
+}
+
+void f(int, int)
+{
+    cout << "This is f(int, int)." << endl;
+}
+
+void f(double, double = 3.14)
+{
+    cout << "This is f(double, double  = 3.14)." << endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    //f(2.56, 42);
+    //二义性。
+    f(42);
+    f(42, 0);
+    f(2.56, 3.14);
+
+    return 0;
+}
