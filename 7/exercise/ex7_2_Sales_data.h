a470a64175d10afd1d39be5dc6542485eeeba07a
roller44 <roller44@163.com>
2016-08-04 10:47:49 +0800

    .

--- 7/exercise/ex7_2_Sales_data.h
+++ 7/exercise/ex7_2_Sales_data.h
@@ -1,6 +1,11 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 #include <string>
+#include <iostream>
+
+using std::cin;
+using std::string;
+
 struct Sales_data {
     std::string bookNo;
     unsigned units_sold = 0;
@@ -11,7 +16,7 @@ struct Sales_data {
         return bookNo;
     }
 
-    Sales_data combine(const Sales_data &rhs)
+    Sales_data& combine(const Sales_data &rhs)
     {
         units_sold += rhs.units_sold;
         revenue += rhs.revenue;
@@ -19,4 +24,7 @@ struct Sales_data {
         return *this;
     }
 };
+
+//std::ostream &print(std::ostream&, const Sales_data&);
+//std::istream &read(std::istream&, Sales_data&);
 #endif
