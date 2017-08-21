748adb85ffb69f409462cec6e7c72ad00ef97de7
Yao Shumin <roller44@163.com>
2017-08-21 17:11:08 +0800

    Add codes of exercise 7.6.

---
+++ 7/exercise/ex7_6_Sales_data.h
@@ -0,0 +1,47 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+#include <string>
+#include <iostream>
+
+using std::cin;
+using std::string;
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+
+    string isbn() const
+    {
+        return bookNo;
+    }
+
+    Sales_data& combine(const Sales_data &rhs)
+    {
+        units_sold += rhs.units_sold;
+        revenue += rhs.revenue;
+
+        return *this;
+    }
+};
+Sales_data add(Sales_data& lhs, Sales_data& rhs)
+{
+	Sales_data sum = lhs;
+	Salse_data.combine(rhs);
+	return sum;
+}
+
+std::ostream &print(std::ostream& os, const Sales_data& item)
+{
+	os << item.bookNo << " " << item.revenue << item.units_sold << endl;
+	return os;
+}
+
+std::istream &read(std::istream& is, Sales_data& item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+#endif
