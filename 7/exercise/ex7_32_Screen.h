1269e2ce72dbab5f9c6d79f72954fbcd18464f32
Yao Shu-Min <roller44@163.com>
2017-08-27 10:51:58 +0800

    Add codes of exercise 7.3.4.

---
+++ 7/exercise/ex7_32_Screen.h
@@ -0,0 +1,69 @@
+#include <string>
+#include <iostream>
+
+class Screen
+{
+	friend void Window_mgr::clear(ScreenIndex);
+
+public:
+	typedef std::string::size_type pos;
+	// alternatetive way to declare a type member using a type alias
+	// using pos = std::string::size_type;
+	Screen() = default; // needed becuase Screen has another constructor
+	// cursor initialized to 0 by its in-class initializer
+	Screen(pos ht, pos wd): height(ht), width(wd), contents(ht * wd, ' ') { }
+	Screen(pos ht, pos wd, char c): height(ht), width(wd), contents(ht * wd, c) { }
+	
+	Screen& display(std::ostream& os)
+	{
+		do_display(os);
+		return *this;
+	}
+	const Screen& display(std::ostream& os) const
+	{
+		do_display(os);
+		return *this;
+	}
+
+	char get() const
+	{
+		return contents[cursor];
+	}
+	inline char get(pos r, pos c) const;
+	Screen& move(pos r, pos c);
+	Screen& set(char c);
+	void some_member() const;
+
+private:
+	pos cursor = 0;
+	pos height = 0;
+	pos width = 0;
+	std::string contents;
+	mutable size_t access_ctr;	// may change even in a const object
+
+	void do_display(std::ostream& os) const
+	{
+		os << contents;
+	}
+};
+
+inline Screen& Screen::move(pos r, pos c)
+{
+	pos row = r * width;
+	cursor = row + c;
+	return *this;
+}
+inline char Screen::get(pos r, pos c) const
+{
+	pos row = r * width;
+	return contents[row + c];
+}
+inline Screen& Screen::set(char c)
+{
+	contents[cursor] = c;
+	return *this;
+}
+void Screen::some_member() const
+{
+	++access_ctr;	// keep a count of the calls to any member function
+}
---
+++ 7/exercise/ex7_32_Window_mgr.h
@@ -0,0 +1,25 @@
+#ifndef EX7_32_WINDOW_MGR
+#define EX7_32_WINDOW_MGR
+#include <vector>
+
+class Window_mgr
+{
+public:
+	// location ID for each screen on the window
+	using ScreenIndex = std::vector<Screen>::size_type;
+	// reset the Screen at the given position to all blanks
+	void clear(ScreenIndex);
+
+private:
+	// Screens this Window_mgr is tracking by default, a Window_mgr has one standard sized blank Screen
+	std::vector<Screen> screens{Scrren(24, 80, ' ')};
+	
+	void Window_mgr::clear(ScreenIndex i)
+	{
+		// s is a reference to the Scrren we want to clear
+		Screen& s = screens[i];
+		// reset the contents of that Screen to all blanks
+		s.contents = string(s.height * s.width, ' ');
+	}	
+};
+#endif
