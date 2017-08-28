6466e8f90de7461f1a4285d3b927bab706752bfc
Yao Shu-Min <roller44@163.com>
2017-08-28 23:27:42 +0800

    Add codes of exercise 7.5.1

---
+++ 7/exercise/ex7_37.cc
@@ -0,0 +1,7 @@
+Sales_data first_item(cin); // Sales_data(std::istream& is) {read(is, *this);}
+
+int main()
+{
+	Sales_data next; // Sales_data(std::string s = ""): bookNo(s) { }
+	Sales_data last("9-999-99999-9"); // Sales_data(std::string s = ""): bookNo(s) { }, bookNo = "9-999-99999-9"
+}
---
+++ 7/exercise/ex7_38_Sales_data.h
@@ -0,0 +1,8 @@
+#ifndef SALES_DATA.H
+#define SALES_DATA.H
+
+class Sales_data
+{
+	Sales_data(std::istream& is = std::cin) {read(is, *this); }
+}
+#endif
